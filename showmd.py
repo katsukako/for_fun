fee9be30da45f2a91644b10d7ed98c0d0a7d69d8
katsu <katsukako668@gmail.com>
2024-01-28 11:10:36 +0900

    clear the screen

--- showmd.py
+++ showmd.py
@@ -1,9 +1,10 @@
-import markdown, time, re
+import markdown, time, re, subprocess
 
 # read .md file
 def read_file(link="../diary_new.md"):
     path =input(f"now {link} to enter to go or new path:")
     link = path if path else link 
+    subprocess.run('clear')
     with open(link, "r", encoding="utf-8") as file:
         show_html = markdown.markdown(file.read())
     return show_html
@@ -17,6 +18,7 @@ def delete_blocks(text):
 # show with 'animation'
 def show(clean_text):
     speed = input('how fast? input a num:')
+    subprocess.run('clear')
     for content in clean_text:
         print(content, end="")
         time.sleep(float(speed))
@@ -28,9 +30,8 @@ def main():
 
 while True:
     try:
-        print()
+        subprocess.run('clear')         # clear the screen
         main()
-        print()
     except KeyboardInterrupt as err:
         print("exit")
         break
